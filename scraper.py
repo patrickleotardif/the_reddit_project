67e1e5b2506defa31f2357baaa4c35b11a7df3d0
Patrick Leo Tardif <plt@rdif.me>
2012-10-28 15:58:27 -0400

    Initial bootstrap function

--- scraper.py
+++ scraper.py
@@ -12,7 +12,8 @@ collection = db.test_collection
 
 def getPage(url) :
 	opener = urllib2.build_opener()
-	opener.addheaders = [('User-agent', 'pltardif scraper for the_reddit_project')]
+	opener.addheaders = [('User-agent', 
+	                      'pltardif scraper for the_reddit_project')]
 	r = opener.open(url)
 	json_data = json.loads(r.read().strip())
 	return json_data['data']	
@@ -25,9 +26,19 @@ def insertFullPage(json_data) :
 		posts.append(item['data'])
 	collection.insert(posts) #bulk insert
 
+def initialBootstrap(numPages) :
+	for i in range(0,numPages):
+		if i == 0:
+			url = 'http://www.reddit.com/.json'
+		else:
+			url = ('http://www.reddit.com/.json?' 
+			       + urllib.urlencode({'count':str(i*25),'after': after}))
+		json_data = getPage(url)
+		insertFullPage(json_data['children'])
+		after = json_data['after']
+		
 def main() :
-	insertFullPage(getPage('http://www.reddit.com/.json')['children'])
-	
+	initialBootstrap(3)
 
 if __name__ == "__main__":
     main()
