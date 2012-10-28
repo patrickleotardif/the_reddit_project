29d7e7daf96bb4817efc0b61edc4a3a8422dd7c9
Patrick Leo Tardif <plt@rdif.me>
2012-10-28 18:25:12 -0400

    Redid elements of bootstrap and defined variable data container

--- scraper.py
+++ scraper.py
@@ -2,13 +2,15 @@ import urllib2
 import urllib
 import json
 import pymongo
+from datetime import datetime
+import calendar
+
 
 #assumes existence of a mongodb connection on the standard port
 from pymongo import Connection
 connection = Connection()
 db = connection.test_database
 collection = db.test_collection
-
 
 def getPage(url) :
 	opener = urllib2.build_opener()
@@ -18,10 +20,23 @@ def getPage(url) :
 	json_data = json.loads(r.read().strip())
 	return json_data['data']	
 
-def insertFullPage(json_data) :
+#UTC time in unix form
+def now():
+	return calendar.timegm(datetime.utcnow().utctimetuple())
+	
+def insertBootstrapPage(json_data,position_reference) :
 	posts = []
 	for item in json_data:
-		item['data']['_id'] = item['data']['id'] #mongo uses _id
+		item['data']['var'] = [{'time':now(),
+		                              'data':{
+		                                'up' :   item['data']['ups'],
+		                                'down' : item['data']['downs'],
+		                                'com' :  item['data']['num_comments'],
+		                                'pos' : (json_data.index(item) 
+		                                        + 1 
+		                                        + position_reference)
+		                              }}]
+		item['data']['_id'] = item['data']['id'] #mongo uses '_id'
 		del item['data']['id'] 
 		posts.append(item['data'])
 	collection.insert(posts) #bulk insert
@@ -29,16 +44,21 @@ def insertFullPage(json_data) :
 def initialBootstrap(numPages) :
 	for i in range(0,numPages):
 		if i == 0:
-			url = 'http://www.reddit.com/.json'
+			url = 'http://www.reddit.com/r/travel/.json?limit=100'
 		else:
-			url = ('http://www.reddit.com/.json?' 
-			       + urllib.urlencode({'count':str(i*25),'after': after}))
-		json_data = getPage(url)
-		insertFullPage(json_data['children'])
-		after = json_data['after']
+			url = ('http://www.reddit.com/r/travel/.json?limit=100&' 
+			       + urllib.urlencode({'count':str(i*100),'after': page['after']}))
+		page = getPage(url)
+		insertBootstrapPage(page['children'],position_reference=i*100)
+
+
+def assignVariableData():
+	#do stuff
+	print 'lala'
 		
 def main() :
-	initialBootstrap(3)
+	initialBootstrap(3) # x* 100 post length pages
+
 
 if __name__ == "__main__":
     main()
