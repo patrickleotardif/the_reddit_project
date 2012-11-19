af96793b60e5e1c034be82b747300547b5ffe5cb
Patrick Leo Tardif <plt@rdif.me>
2012-11-18 23:18:07 -0500

    More analysis

--- analysis.py
+++ analysis.py
@@ -7,14 +7,15 @@ from pymongo import Connection
 connection = Connection()
 db = connection.reddit
 
-collection = db.fitness
+collection = db.travel
 
-START_TIME = 1352955347
+START_TIME = 1352938569
 #TRAVEL = 1352938569
 #FITNESS = 1352955347
 #####################
 
 parms = {'created_utc':{'$gte' : START_TIME}}
+parms2 = {'created_utc':START_TIME}
 
 def maxRankAchieved():	
 	ranks = {}
@@ -30,6 +31,38 @@ def maxRankAchieved():
 				ranks[s] = [doc['_id']]
 	plt.plot(map(lambda x: len(ranks[x]),ranks))
 	plt.show()
+
+def completion_matrix():
+	#Get the time points to be used
+	m = collection.find_one(parms2,{'var.time'})['var']
+	new_m = []
+	for i in range(0,len(m)):
+		if i % 10 == 0 :
+			new_m.append(m[i])
+	m = new_m
 	
+	counter = len(m)
+	for pt in m:
+		counter -= 1
+		pos = []
+		print counter
+		for doc in collection.find(parms):
+			for docpt in doc['var']:
+				if docpt['time'] > (pt['time']-30) and docpt['time'] < (pt['time']+30):
+					if docpt['data'] != '?' : 
+						pos.append(docpt['data']['pos'])
+						break;
+		pt['pos'] = pos
+	
+	img = []
+	for item in m:
+		row = [0] * 100
+		for p in item['pos']:
+			if p <= 100 : row[p-1] = 1
+		img.append(row)
+	plt.imshow(img)
+	plt.show()
 
-
+		
+				
+		
