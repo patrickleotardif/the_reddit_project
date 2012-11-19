3a5f496bd33f474dd7560862c039d782f11b0423
Patrick Leo Tardif <plt@rdif.me>
2012-11-18 20:59:14 -0500

    More cleaning/removing

--- data/R graphing.R
+++
@@ -1,8 +0,0 @@
-R graphing
-
-
-row <- df[[25]]; plot(row$time,row$pos,type='l',ylim=rev(c(1,50)),xlim=c(0,5000))
-for(i in 2:length(df)) {
-    row <- df[[i]]
-    lines(row$time,row$pos,type='l',ylim=rev(c(1,50)),xlim=c(0,600))
-}
